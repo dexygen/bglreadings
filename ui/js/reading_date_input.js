ed41e4179eaf0b5eae51a41c01f0027e480000f5
George Jempty <consult@dexygen.com>
2019-01-24 07:40:01 +0000

    More robust solution for setting default picker date avoiding UTC pitfalls

--- ui/js/reading_date_input.js
+++ ui/js/reading_date_input.js
@@ -1,7 +1,12 @@
 class ReadingDateInput extends React.Component {
   componentDidMount() {
     this.refs.dateInput.focus();
-    this.refs.dateInput.value = (new Date()).toISOString().substr(0,10);
+    this.refs.dateInput.value = (function() { //see: https://stackoverflow.com/a/54341296/34806
+      let now = new Date();
+      let month = (now.getMonth() + 1).toString().padStart(2, '0');
+      let day = now.getDate().toString().padStart(2, '0');
+      return `${now.getFullYear()}-${month}-${day}`;
+    })();
   }
   
   updateDate = () => {
